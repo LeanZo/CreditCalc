fca79b55419110f534c13494f7fae162c595c305
LeanZo <kashchannel@live.com>
2016-10-24 19:12:27 -0200

    BETA 1.0
    Ja pode ser usado para calcular o limite do cartão, porem possui bugs e
    design nao-trabalhado

--- CreditCalc/Program.cs
+++ CreditCalc/Program.cs
@@ -252,66 +252,171 @@ namespace CreditCalc
         static void Consultar(string today, string FSdocPath)
         {
             Console.Clear();
-            Console.WriteLine("Qual ano deseja verificar?");
+            Console.WriteLine("Selecione o que deseja mostrar:");
+            Console.WriteLine("1 - Tudo");
+            Console.WriteLine("2 - Mês/Ano Especifico");
             Console.Write(">");
-            string anoresp = Console.ReadLine();
-            Console.Clear();
-            Console.WriteLine("Ano: {0}", anoresp);
-            Console.WriteLine("Qual mês deseja verificar?");
-            Console.Write(">");
-            string mesresp = Console.ReadLine();
-            Console.Clear();
-            Console.WriteLine("Consultando: {0}/{1}", mesresp, anoresp);
-            Console.WriteLine();
-            string datetemp = @"01/" + mesresp + @"/" + anoresp;
-            string databruta = datetemp;
-            DateTime dataliquida = Convert.ToDateTime(databruta);
-            DateTime dataTimeArray = new DateTime();
-            string dataArraystring = "undefined";
-
-            int count = 0;
-            int count2 = 0;
-            var textLines = File.ReadAllLines(FSdocPath);
-            string[] dataArray = new string[textLines.Length];
-            foreach (var line in textLines)
+            int respconsulta = 0;
+            bool interruptorconsulta = true;
+            do
             {
-                for (int i = 0; i < dataArray.Length; i++)
+                try
                 {
-                        dataArray = line.Split(',');
-                    if (count2 == 0)
+                    respconsulta = Convert.ToInt32(Console.ReadLine());
+                    if (respconsulta == 1 || respconsulta == 2)
                     {
-                        dataArraystring = Convert.ToString(dataArray[i]);
-                        dataTimeArray = Convert.ToDateTime(dataArraystring);
+                        interruptorconsulta = false;
                     }
-                   if (dataTimeArray.Year == dataliquida.Year && dataTimeArray.Month == dataliquida.Month)
-                        {
-                        Console.Write(dataArray[i]);
-
-                        Console.Write("      ");
-                        count++;
-                        count2++;
-                        if ((count % 3) == 0)
-                        {
-                            Console.WriteLine();
-                            count2 = 0;
-                        }
-                    } else
+                    else
                     {
-                        count++;
-                        count2++;
-                        if ((count % 3) == 0)
+                        respconsulta = Convert.ToInt32("Exception.Forcer");
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Invalido! Tenha certeza de digitar corretamente");
+                    Console.WriteLine("Enter para continuar...");
+                    Console.ReadLine();
+                    Console.Clear();
+                    Console.WriteLine("Selecione o que deseja mostrar:");
+                    Console.WriteLine("1 - Tudo");
+                    Console.WriteLine("2 - Mês/Ano Especifico");
+                    Console.Write(">");
+                }
+            } while (interruptorconsulta);
+            Console.Clear();
+                switch (respconsulta)
+                {
+                    case 1:
+                        int countall = 0;
+                        int count2all = 0;
+                        int count3all = 0;
+                        double valoresall = 0;
+                        double limiteall = 0;
+                        var textLinesall = File.ReadAllLines(FSdocPath);
+                        string[] dataArrayall = new string[textLinesall.Length];
+                        foreach (var line in textLinesall)
                         {
-                            count2 = 0;
+                            if (count3all != 0)
+                            {
+                                for (int i = 0; i < dataArrayall.Length; i++)
+                                {
+                                    dataArrayall = line.Split(',');
+                                    if (count2all == 2)
+                                    {
+                                        valoresall += Convert.ToDouble(dataArrayall[i].Replace('.', ','));
+                                    }
+                                    Console.Write(dataArrayall[i]);
+                                    Console.Write("      ");
+                                    countall++;
+                                    count2all++;
+                                    if ((countall % 3) == 0)
+                                    {
+                                        Console.WriteLine();
+                                        count2all = 0;
+                                    }
+                                }
+                            }
+                            else
+                            {
+                                limiteall = Convert.ToDouble(line.Replace('.', ','));
+                                count3all++;
+                            }
                         }
+                        Console.WriteLine();
+                        Console.WriteLine("Total gasto: R$ {0}", valoresall);
+                        Console.WriteLine();
+                        Console.WriteLine("Enter: Voltar");
+                        Console.ReadKey();
+                        Start(today, FSdocPath);
+                        break;
+                    case 2:
+                        Console.WriteLine("Qual ano deseja verificar?");
+                        Console.Write(">");
+                        string anoresp = Console.ReadLine();
+                        Console.Clear();
+                        Console.WriteLine("Ano: {0}", anoresp);
+                        Console.WriteLine("Qual mês deseja verificar?");
+                        Console.Write(">");
+                        string mesresp = Console.ReadLine();
+                        Console.Clear();
+                        Console.WriteLine("Consultando: {0}/{1}", mesresp, anoresp);
+                        Console.WriteLine();
+                        string datetemp = @"01/" + mesresp + @"/" + anoresp;
+                        string databruta = datetemp;
+                        DateTime dataliquida = Convert.ToDateTime(databruta);
+                        DateTime dataTimeArray = new DateTime();
+                        string dataArraystring = "undefined";
+                        int count = 0;
+                        int count2 = 0;
+                        int count3 = 0;
+                        double valores = 0;
+                        double limite = 0;
+                        var textLines = File.ReadAllLines(FSdocPath);
+                        string[] dataArray = new string[textLines.Length];
+                        foreach (var line in textLines)
+                        {
+                            if (count3 != 0)
+                            {
+                                for (int i = 0; i < dataArray.Length; i++)
+                                {
+                                    dataArray = line.Split(',');
+                                    if (count2 == 0)
+                                    {
+                                        dataArraystring = Convert.ToString(dataArray[i]);
+                                        dataTimeArray = Convert.ToDateTime(dataArraystring);
+                                    }
+                                    if (count2 == 2)
+                                    {
+                                        valores += Convert.ToDouble(dataArray[i].Replace('.', ','));
+                                    }
+                                    if (dataTimeArray.Year == dataliquida.Year && dataTimeArray.Month == dataliquida.Month)
+                                    {
+                                        Console.Write(dataArray[i]);
 
-                    }
-                    }
+                                        Console.Write("      ");
+                                        count++;
+                                        count2++;
+                                        if ((count % 3) == 0)
+                                        {
+                                            Console.WriteLine();
+                                            count2 = 0;
+                                        }
+                                    }
+                                    else
+                                    {
+                                        count++;
+                                        count2++;
+                                        if ((count % 3) == 0)
+                                        {
+                                            count2 = 0;
+                                        }
 
-            }
-            Console.WriteLine();
-            Console.WriteLine("Enter: Voltar");
-            Console.ReadKey();
-            Start(today, FSdocPath);
+                                    }
+                                }
+                            }
+                            else
+                            {
+                                limite = Convert.ToDouble(line.Replace('.', ','));
+                                count3++;
+                            }
+
+                        }
+                        Console.WriteLine();
+                        if (limite != 0)
+                        Console.WriteLine("O seu limite é: R$ {0}", limite);
+                        Console.WriteLine("Você ja gastou: R$ {0}", valores);
+                        if (limite != 0)
+                        Console.WriteLine("Restam: R$ " + (limite - valores));
+                        Console.WriteLine();
+                        Console.WriteLine("Enter: Voltar");
+                        Console.ReadKey();
+                        Start(today, FSdocPath);
+                        break;
+                    default:
+
+                        break;
+                }
         }
 
 
@@ -319,9 +424,11 @@ namespace CreditCalc
         {
             string today = DateTime.Now.ToString("dd/MM/yyyy");
             string month = DateTime.Now.Month.ToString();
-            Console.WriteLine(today);
+            Console.WriteLine(today + "- BETA 1.0");
             string FSdocPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), @"CreditCalc/CreditCalc.fsdoc");
             FileStream FSdoc;
+            string limiteresp = "undefined";
+            bool limiteinterruptor = true;
             if (File.Exists(FSdocPath))
             {
                 FSdoc = new FileStream(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), @"CreditCalc/CreditCalc.fsdoc"), FileMode.Open, FileAccess.ReadWrite);
@@ -331,13 +438,36 @@ namespace CreditCalc
             {
                 Console.WriteLine("Arquivo 'CreditCalc.fsdoc' não foi encontrado");
                 Console.WriteLine("Criando um novo arquivo 'CreditCalc.fsdoc'");
+                Console.WriteLine("Enter para continuar...");
+                Console.ReadKey();
+                Console.Clear();
+                Console.WriteLine("Qual o limite mensal do seu cartão (Deixe em branco caso não tenha limite)");
+                Console.Write("R$: ");
+                do
+                {
+                    try
+                    {
+                        limiteresp = Console.ReadLine().Replace(',','.');
+                        if (String.IsNullOrEmpty(limiteresp))
+                            limiteresp = "0";
+                        Convert.ToDouble(limiteresp);
+                        limiteinterruptor = false;
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine("Você tentou inserir um valor invalido, insira apenas numeros");
+                        Console.Write("R$: ");
+                    }
+                } while (limiteinterruptor);
                 Directory.CreateDirectory(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "CreditCalc"));
                 using (File.Create(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), @"CreditCalc/CreditCalc.fsdoc"))) { }
                 Console.WriteLine("...");
                 FSdoc = new FileStream(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), @"CreditCalc/CreditCalc.fsdoc"), FileMode.Open, FileAccess.ReadWrite);
-                Console.WriteLine("Feito!");
             }
             FSdoc.Close();
+            if(limiteresp != "undefined")
+            File.AppendAllText(FSdocPath, limiteresp + Environment.NewLine);
+            Console.WriteLine("Feito!");
             Console.WriteLine("Enter para continuar...");
             Console.ReadKey();
             Console.Clear();
